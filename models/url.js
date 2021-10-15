0776969310cfd5d2f7a5c6da6c7237046a185543
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-15 11:51:38 +0800

    feat: add url model

---
+++ models/url.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const urlSchema = new Schema({
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('url', urlSchema)
