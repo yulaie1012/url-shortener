0a975b67c073b5a834dbf4331717d9dee1c294bb
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-15 14:55:43 +0800

    feat: add post / route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(routes)
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.post('/', (req, res) => {
+  res.render('index', req.body)
+})
+
 module.exports = router
