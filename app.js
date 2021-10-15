f0b36c150bb76a2f425b4f10a549195a02f076b4
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-15 13:54:20 +0800

    feat: handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
 const routes = require('./routes')
 require('./config/mongoose')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
