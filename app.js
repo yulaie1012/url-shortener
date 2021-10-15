5f86cbcd05b8a7d6be535254a38f5bfe749499d0
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-15 11:38:15 +0800

    feat: add express.Router

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const routes = require('./routes')
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
